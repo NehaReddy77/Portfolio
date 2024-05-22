778d66a75f30401e39f9ae88f6c340e719bd4526
NehaReddy77 <nehareddy315@gmail.com>
2024-05-22 18:57:35 -0400

    Updates

---
+++ precache-manifest.cadafbce88473fe3ef274920f7021c3b.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3f185f0ebf0cc573dca93bd13648f67",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3c72ffc4cdc835c1f8b7",
+    "url": "/portfolio/static/css/2.6a416308.chunk.css"
+  },
+  {
+    "revision": "17a77acfbb0aac6a0b10",
+    "url": "/portfolio/static/css/3.c508d8b9.chunk.css"
+  },
+  {
+    "revision": "c280a29f02fe360e0f36",
+    "url": "/portfolio/static/css/4.f0b7a320.chunk.css"
+  },
+  {
+    "revision": "ac52bda656c80455e1fb",
+    "url": "/portfolio/static/css/main.528a651c.chunk.css"
+  },
+  {
+    "revision": "3c72ffc4cdc835c1f8b7",
+    "url": "/portfolio/static/js/2.38e1928b.chunk.js"
+  },
+  {
+    "revision": "17a77acfbb0aac6a0b10",
+    "url": "/portfolio/static/js/3.cded93cb.chunk.js"
+  },
+  {
+    "revision": "c280a29f02fe360e0f36",
+    "url": "/portfolio/static/js/4.5cdb3d0e.chunk.js"
+  },
+  {
+    "revision": "ac52bda656c80455e1fb",
+    "url": "/portfolio/static/js/main.2450d14d.chunk.js"
+  },
+  {
+    "revision": "027f9a3fe6278f8cf0ab",
+    "url": "/portfolio/static/js/runtime-main.d5703df3.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "4a50b1b3a08c00d85e880457147cb432",
+    "url": "/portfolio/static/media/aws-logo.4a50b1b3.png"
+  },
+  {
+    "revision": "d0a70bf39f1215c457e760e598e7bd6f",
+    "url": "/portfolio/static/media/cognizant-logo.d0a70bf3.png"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/portfolio/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "a41f784840703e7c39e9d5ff37133c77",
+    "url": "/portfolio/static/media/developerActivity.a41f7848.svg"
+  },
+  {
+    "revision": "b55b1345f0b919f0cab774ec25d6654e",
+    "url": "/portfolio/static/media/fa-brands-400.b55b1345.woff2"
+  },
+  {
+    "revision": "b7dee83cb5ee2c47b053e2620f4bbb78",
+    "url": "/portfolio/static/media/fa-brands-400.b7dee83c.ttf"
+  },
+  {
+    "revision": "3c264849ff4eb9b6e99eab9cd54c80ae",
+    "url": "/portfolio/static/media/fa-regular-400.3c264849.ttf"
+  },
+  {
+    "revision": "aa7c5fa494807f7a9ec907defee083e8",
+    "url": "/portfolio/static/media/fa-regular-400.aa7c5fa4.woff2"
+  },
+  {
+    "revision": "0a95f951745ba02faa8773ea6a1ebaed",
+    "url": "/portfolio/static/media/fa-solid-900.0a95f951.ttf"
+  },
+  {
+    "revision": "1ec0ba058c021acf7feaa18081445d63",
+    "url": "/portfolio/static/media/fa-solid-900.1ec0ba05.woff2"
+  },
+  {
+    "revision": "95b97efa98f9e3fb869bc9634c43a0cc",
+    "url": "/portfolio/static/media/fa-v4compatibility.95b97efa.ttf"
+  },
+  {
+    "revision": "fdb652dcc200dd23b8b8040176858c36",
+    "url": "/portfolio/static/media/fa-v4compatibility.fdb652dc.woff2"
+  },
+  {
+    "revision": "d6a2dcd5cb04a2d44cca99b6acb5da7e",
+    "url": "/portfolio/static/media/ibm_logo.d6a2dcd5.png"
+  },
+  {
+    "revision": "b1ae2adc77bafd0ab3f670d9ab06b5ca",
+    "url": "/portfolio/static/media/jsFramework.b1ae2adc.svg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/portfolio/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "a15c230b794fda4eb28f2320529072a1",
+    "url": "/portfolio/static/media/manOnTable.a15c230b.svg"
+  },
+  {
+    "revision": "d73bb2bcddad315c67b738e10a345582",
+    "url": "/portfolio/static/media/meta_pic.d73bb2bc.png"
+  },
+  {
+    "revision": "7a6c4841a2faec9e578b096408e4cd34",
+    "url": "/portfolio/static/media/ncr-logo.7a6c4841.png"
+  },
+  {
+    "revision": "f041d99793f0c15a757a3d8b73d937a8",
+    "url": "/portfolio/static/media/pierian-data-logo.f041d997.png"
+  },
+  {
+    "revision": "8bde57f4df480d5b2c66f6af0bb7f62d",
+    "url": "/portfolio/static/media/profile.8bde57f4.jpg"
+  },
+  {
+    "revision": "8eef3438717cbacae341a4b5cfbea43f",
+    "url": "/portfolio/static/media/profile2.8eef3438.png"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/portfolio/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "05b89315da86d827606f827e50c8e7ca",
+    "url": "/portfolio/static/media/rahulshettyacademy-logo.05b89315.png"
+  },
+  {
+    "revision": "1591c202ea86be03349f8d565ac1f959",
+    "url": "/portfolio/static/media/seetest-logo.1591c202.png"
+  },
+  {
+    "revision": "96c9d7a913fc06de9bd7e13f5cb26af9",
+    "url": "/portfolio/static/media/skill.96c9d7a9.svg"
+  },
+  {
+    "revision": "c4daca80e09c955cdba1a3a8cbc104ca",
+    "url": "/portfolio/static/media/su.c4daca80.png"
+  },
+  {
+    "revision": "9e7ceaae7c1ca27d162a0b61a12a8bd5",
+    "url": "/portfolio/static/media/su2.9e7ceaae.png"
+  },
+  {
+    "revision": "c3a2b12964d7d0f89c0d3ac043330eb8",
+    "url": "/portfolio/static/media/su3.c3a2b129.png"
+  },
+  {
+    "revision": "0b16d359023a22b371104d7e4a15d32c",
+    "url": "/portfolio/static/media/tcs-logo.0b16d359.png"
+  },
+  {
+    "revision": "6c7b47a7b29904919f175780d51a1ec2",
+    "url": "/portfolio/static/media/vc.6c7b47a7.png"
+  },
+  {
+    "revision": "7ebc764bf5f86dd0e65964bc4a734411",
+    "url": "/portfolio/static/media/vskills-logo.7ebc764b.png"
+  },
+  {
+    "revision": "68650cfe6fc9377a581ebbefb7b1a6ad",
+    "url": "/portfolio/static/media/woman.68650cfe.png"
+  },
+  {
+    "revision": "d0a70af7f35f5943c7e28883adbbb7fd",
+    "url": "/portfolio/static/media/zero-to-mastery.d0a70af7.png"
+  }
+]);
